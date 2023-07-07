42f08615082c9969954aa7b0f5730e783e13b730
nkatha1 <nkathapatience1@gmail.com>
2023-07-07 12:31:24 -0700

    5

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
+ Binary file addedBIN +1.46 KB 
+0x09-static_libraries/1-strncat.o
