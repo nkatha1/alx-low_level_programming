af1ec4d5eb841f26be777309996683b730b026dd
nkatha1 <nkathapatience1@gmail.com>
2023-09-04 03:04:46 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,38 @@
 #include "main.h"
 
 /**
- * create_and_write_file - Creates a new file and
- *                       writes specified text content to it.
- * @file_name: The name of the file to create.
- * @content: The text content to write to the file.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
-int create_and_write_file(const char *file_name, char *content)
+int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int num_characters;
-	int bytes_written;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (!file_name)
+	if (!filename)
 		return (-1);
 
-	file_descriptor = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_descriptor == -1)
+	if (fd == -1)
 		return (-1);
 
-	if (!content)
-		content = "";
+	if (!text_content)
+		text_content = "";
 
-	for (num_characters = 0; content[num_characters]; num_characters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	bytes_written = write(file_descriptor, content, num_characters);
+	rwr = write(fd, text_content, nletters);
 
-	if (bytes_written == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(file_descriptor);
+	close(fd);
 
 	return (1);
 }
-
